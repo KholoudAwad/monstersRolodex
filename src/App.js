28e0d84210e74fda5809cfe9a2e1e31fde66525d
Kholoud Awad <kholoudmawad@gmail.com>
2021-06-01 22:50:36 -0400

    adding changes to the pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import {CardList} from './Components/card-list/card-list.component'
+import {SearchBox} from './Components/searchBox/search-box.component'
 
-function App() {
+class App extends Component {
+
+constructor(){
+  super();
+  this.state={
+   monsters:[],
+   searchField:''
+  };
+}
+
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
+
+
+  render(){
+    const {monsters , searchField} =this.state;
+    const filteredMonsters = monsters.filter( monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+         placeholder="Search monster" 
+         handleChange={e => this.setState({searchField:e.target.value})}
+        />
+        <CardList monsters={filteredMonsters} > </CardList>
+      </div>
+    );
+  }
+}
+
+
+/* <p>
+        {this.state.string}
+        </p>
+       <button onClick={()=>this.setState({string:'Helloooo Again'})}>Change text</button> */
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +58,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn....
         </a>
       </header>
     </div>
   );
-}
+} */
 
 export default App;
